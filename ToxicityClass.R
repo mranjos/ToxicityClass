606825fee4996bc9332950d534974260c0f8f16c
mranjos <mranjos91@gmail.com>
2019-05-21 00:16:11 -0300

    Update codígos.

--- ToxicityClass.R
+++ ToxicityClass.R
@@ -8,48 +8,176 @@ if(length(inst_pkgs)){
 
 lapply(load_pkgs, require, character.only=T)
 
-setwd(" ")
-dados=fread("train.csv",header = TRUE)
-head(dados)
-#creat corpus of Ingredients(TEXT)
-corpus=Corpus(VectorSource(dados$comment_text))
+setwd("E:\\1 - Estudos\\1 - Projetos\\Kaggle\\Analise de sentimento\\Dados")
+train = fread("train.csv",header = TRUE, encoding = "UTF-8")
+test = fread("test.csv",header = TRUE, encoding = "UTF-8")
+
+#######################################################################
+
+
+
+
+
+
+
+
+
+
+############# LIMPANDO OS train #############
+
+#creat corpus_train of Ingredients(TEXT)
+corpus_train = Corpus(VectorSource(train$comment_text))
+corpus_test  = Corpus(VectorSource(test$comment_text))
+
+writeLines(as.character(corpus_train[[1]])) # Verificando os comentarios carregados
+writeLines(as.character(corpus_test[[1]])) # Verificando os comentarios carregados
+
+#######################################################################
+
+
+
+
+
+
+
+
+
+
+############# TRATANDO OS DADOS DE TREINAMENTO
+
 #convert text lowercase
-corpus=tm_map(corpus,tolower)
-corpus[[1]]
+corpus_train = tm_map(corpus_train,content_transformer(tolower))
 #remove Punctuation
-corpus=tm_map(corpus,removePunctuation)
+corpus_train = tm_map(corpus_train,removePunctuation)
 #Remove Stopwords
-corpus=tm_map(corpus,removeWords,c(stopwords('english')))
+corpus_train = tm_map(corpus_train,removeWords,c(stopwords('english')))
 #Remove Whitespaces
-corpus=tm_map(corpus,stripWhitespace)
+corpus_train = tm_map(corpus_train,stripWhitespace)
+#Removendo numeros
+corpus_train = tm_map(corpus_train, removeNumbers) 
 #Perform Stemming
-corpus=tm_map(corpus,stemDocument)
-#transformando em texto (nao sei se é necessário)
-corpus=tm_map(corpus,PlainTextDocument)
+#corpus_train = tm_map(corpus_train,stemDocument)
+
+#######################################################################
 
 
-###document matrix
-frequencies = DocumentTermMatrix(corpus)
-frequencies
 
 
-#organizando frequency of terms
-freq=colSums(as.matrix(frequencies))
-length(freq)
-ord=order(freq)
-ord
 
-#if wish to export the matrix (to see how it looks) to an excel file
-m=as.matrix(frequencies)
-dim(m)
-write.csv(m,file='matrix.csv')
 
-#check most and least frequent words
-freq[head(ord)]
-freq[tail(ord)] 
-#check our table of 20 frequencies
-head(table(freq),20)
-tail(table(freq),20)
+
+
+
+
+############# TRATANDO OS DADOS DE TESTE
+
+#convert text lowercase
+corpus_test = tm_map(corpus_test,content_transformer(tolower))
+#remove Punctuation
+corpus_test = tm_map(corpus_test,removePunctuation)
+#Remove Stopwords
+corpus_test = tm_map(corpus_test,removeWords,c(stopwords('english')))
+#Remove Whitespaces
+corpus_test = tm_map(corpus_test,stripWhitespace)
+#Removendo numeros
+corpus_test = tm_map(corpus_test, removeNumbers)
+
+#######################################################################
+
+
+
+
+
+
+
+
+
+
+############# TRANSFORMANDO A MATRIZ EM DOCUMENTO TERMO
+
+#frequencies = DocumentTermMatrix(corpus_train)
+#frequencies
+
+dtm_train = DocumentTermMatrix(corpus_train)
+inspect(dtm_train[1:100, 1:50])
+
+dtm_test = DocumentTermMatrix(corpus_test)
+inspect(dtm_test[1:5, 1:20])
+
+#######################################################################
+
+
+
+
+
+
+
+
+
+
+############# VERIFICANDO A FREQUÊNCIA DAS PALAVRAS 2 (Quando não conseguimos transformar em Matrix)
+
+#freq = findFreqTerms(dtm_train,1000) #Encontrando termos que aparecem mais que 1000 vezes
+
+comentarios = train$comment_text
+avisos1 = tolower(comentarios)
+avisos1 = removePunctuation(avisos1)
+avisos1 = removeNumbers(avisos1)
+avisos1 = removeWords(avisos1, stopwords("english"))
+avisos1 = stripWhitespace(avisos1)
+avisos1 = trimws(avisos1)
+avisos1 = strsplit(avisos1, " ")
+avisos1 = unlist(avisos1)
+
+
+freq  = table(avisos1)
+freq1 = sort(freq, decreasing=TRUE)
+temple.sorted.table = data.frame(Freq = freq1)
+
+teste = head(temple.sorted.table, 30) # 30 palavras mais frequentes
+
+
+##### Nuvem palavras 
+
+pal1 <- brewer.pal(30,"Paired")
+pal3 <- brewer.pal(30,"Set3")
+
+## Nuvem de palavras mais frequentes
+WC <- wordcloud(temple.sorted.table$Freq.avisos1, 
+                temple.sorted.table$Freq.Freq,
+                min.freq=1,
+                max.words=30, 
+                random.order=FALSE, 
+                rot.per=.15,colors=pal1)
+
+#######################################################################
+
+
+
+
+
+
+
+
+
+############# PALAVRAS TÓXICAS
+
+vetor_toxic = c("shit","fuck","pussy","dick","damn","bitch","crap","piss","darn","cock","asshole","bastard","fag","Arse","Arsehole","slut","douche","Ass","Berk","Crickhold")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #remove sparse terms
 
@@ -89,4 +217,4 @@ dim(newsparse)
 colnames(newsparse)=make.names(colnames(newsparse))
 
 #check for the dominant dependent variable
-table(dados$cuisine)
+table(train$cuisine)
